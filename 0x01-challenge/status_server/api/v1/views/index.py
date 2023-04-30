73769bc63f330175e671f286f9ed692cfe8f3545
adamsbola <adamadu52@gmail.com>
2023-04-30 18:07:50 +0000

    0x01-challenge

--- 0x01-challenge/status_server/api/v1/views/index.py
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -3,7 +3,7 @@
 """
 from flask import jsonify
 
-from api.v1.views import app_views
+from status_server.api.v1.views import app_views
 
 
 @app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
